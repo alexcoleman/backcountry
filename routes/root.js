540b89b3502d3cb012bb9de75c811e23aa7f7f3e
Crystal Chang <crystalschang@gmail.com>
2013-03-22 17:42:40 -0600

    Base pages

--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = EXPRESS_APP,
     hoganEngine = require('hogan-engine');
 
-// Jeans
+/** Home **/
 app.get('/', function (req, res, next) {
   var context = {
     page: {
@@ -11,4 +11,26 @@ app.get('/', function (req, res, next) {
   };
 
   res.render('home', context);
+});
+
+/** User **/
+app.get('/user/:id', function (req, res, next) {
+  var context = {
+    page: {
+      title: "User page"
+    }
+  };
+
+  res.render('user', context);
+});
+
+/** Destination **/
+app.get('/destination/:id', function (req, res, next) {
+  var context = {
+    page: {
+      title: "Destination page"
+    }
+  };
+
+  res.render('destination', context);
 });
