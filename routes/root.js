129edbf6aef5bde2330f461a44db95ca2e4d7d19
dylanbathurst <dylanbathurst@gmail.com>
2013-03-22 19:36:00 -0600

    adding seasonal gear partial

--- routes/root.js
+++ routes/root.js
@@ -32,7 +32,7 @@ app.get('/user/:id', function (req, res, next) {
 app.get('/destination/:id', function (req, res, next) {
   var context = {
     css: [{href: '/css/styles.css'}, {href: '/css/bootstrap.min.css'}, {href: 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css'}],
-    js: [{src: '/js/jquery.min.js'}, {src: '/js/modernizr.min.js'}, {src: '/js/bootstrap.js'}, {src: 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js'}, {src: '/js/mapCode.js'}],
+    js: [{src: '/js/jquery.min.js'}, {src: '/js/modernizr.min.js'}, {src: '/js/bootstrap.js'}, {src: 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js'}, {src: '/js/mapCode.js'}, {src: '/js/destination.js'}],
     page: {
       title: "Destination page"
     }
